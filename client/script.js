991c86ec7d3a1864710bafe1160ad37ac5a2822d
owsym <ocamafarooq.97@gmail.com>
2023-08-11 22:29:36 +0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://localhost:5000', {
+  const response = await fetch('https://x-kbj8.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
